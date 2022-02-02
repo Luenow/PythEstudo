a08f7703e57c411468a4ccbdcb220713e7448e4c
Luenow <lukarsan@gmail.com>
2022-02-02 19:46:46 -0300

    Estudo Python

---
+++ Exercícios/ex006.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+d = n1 + 2
+t = n1 * 3
+r = n1** (1/2)
+print('O dobro de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} é igual a {:.3f}'.format(n1, d, n1, t, n1, r))
---
+++ dio/aula9.py
@@ -0,0 +1,10 @@
+def escrever_arquivo(texto):
+    arquivo = open('teste.txt', 'a')
+    arquivo.write('\nTerceira linha')
+    arquivo.close()
+
+def atualizar_arquivo(texto):
+    arquivo = open('teste.txt', 'a')
+    arquivo.write('\nTerceira linha')
+    arquivo.close()
+
