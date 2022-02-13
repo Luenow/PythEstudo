afefedfdc00aed7546256e0a8e72a73239d3b680
Luenow <lukarsan@gmail.com>
2022-02-13 20:52:33 -0300

    Estudo Python

---
+++ Exercícios/ex013.py
@@ -0,0 +1,3 @@
+Salario = float(input('Qual é o salário do Funcionário? R$'))
+#prom = Salario + (Salario*15/100)
+print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(Salario, Salario + (Salario*15/100)))
