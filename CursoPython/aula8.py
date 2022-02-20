1a5ebbce7a3667db5709ce7b123f3abf7a82c4b3
Luenow <lukarsan@gmail.com>
2022-02-20 18:10:13 -0300

    Estudo Python

---
+++ CursoPython/aula8.py
@@ -0,0 +1,18 @@
+import random
+num = random.randint(1, 10)
+print(num)
+
+
+
+
+
+
+
+
+
+
+
+#from math import sqrt, floor
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print('A raiz de {} é igual a {:.2f}'.format(num, floor(raiz)))
---
+++ Solyd/Aula3.py
@@ -0,0 +1,16 @@
+# Comparacoes: == !=  >  <  >=  <=
+# Comparacoes: and  or
+
+#a = 50
+#b = 20
+
+
+#if a > b and 'abacaxi' == 'uva':
+#    print(a, 'Dois é maior do que um', b)
+#else:
+#    print(a, 'Não é maior do que', b)
+
+idade = 50
+
+if idade == 50:
+    print('Você tem 50 anos')
