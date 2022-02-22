159971efa0426646fd724d53d121ca2849e720ed
Luenow <lukarsan@gmail.com>
2022-02-22 19:24:05 -0300

    Estudo Python

--- Solyd/Aula4.py
+++ Solyd/Aula4.py
@@ -1,2 +1,5 @@
-frase = 'oi, tudo bem?'
-lista_nomes = ['Joao', 'Maria', 'Guilerme', 'Diego']
+frase = 'Oi, tudo bem?'
+lista_nomes = ['Joao', 'Maria', 'Guilerme', 'Diego', 'Joao']
+
+frase_nova = frase + 'Como vai você?'
+print(frase_nova)
